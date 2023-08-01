764ce67cb971cdd7044c2af431cdd2d0209a6347
Usmonaliyev Oktam <oktamgithub@gmail.com>
2023-08-01 13:30:01 +0500

    JS 1-dars
    22-guruh

---
+++ js/main.js
@@ -0,0 +1,57 @@
+/*console.log(2+2-5*4);*/
+/*alert("Saytimizga xush kelibsiz!");*/
+/*document.write("Men body oynasida ko'rinaman!");*/
+
+/*let x = 2;*/
+/*let y = 5;*/
+/*x = 10;*/
+
+/*let x = 2, y = 5, z = 8;*/
+/*document.write(x+y+z);*/
+
+/*let salom = "Assalomu alaykum!";*/
+/*let rang = "#f1f1f1";*/
+/*document.write(rang);*/
+
+/* Data types in JS */
+/*let x = 5;*/                  /* number */
+/*let y = "JavaScript";*/       /* string */
+/*document.write(typeof y);*/
+
+/* Math in JS */
+/*let x = 55;*/
+/*let matn = "20";*/
+/*x+=1;*/ /* x=5+1 */
+/*x-=1;*/ /* x=5-1 */
+/*x*=1;*/ /* x=5*1 */
+/*x/=1;*/   /* x=5/1 */
+/*let qoldiq = x%4;*/
+/*let daraja = x**2;*/      /* 5*5 */
+/*++x;*/
+/*--x;*/
+/*x++;*/
+/*x--;*/
+/*document.write(x-matn);*/
+
+/* if (agar) */
+/* else (u holda) */
+/* else if (yoki bo'lmasa) */
+
+/*if (44 == 45){
+    console.log("Ular teng!");
+}*/
+/*else {
+    console.log("Ular teng emas!")
+}*/
+
+if (45 == 44){
+    console.log("Birinchisidagi qiymatlar teng!");
+}
+else if (45 == 46){
+    console.log("Ikkinchisidagi qiymatlar teng!");
+}
+else {
+    console.log("Ikkalasidagi qiymatlar teng emas!")
+}
+
+
